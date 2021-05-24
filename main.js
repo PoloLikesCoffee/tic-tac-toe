ca96cc8bfe8f775d8293f968d3b9d9bd0e9beace
PoloLikesCoffee <phglorianne@gmail.com>
2021-05-24 18:57:03 +0200

    change css, add responsive, instead of one module, now two modules, fix disabled cell when win game

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ const Player = (name, marker, turn) => {
 };
 
 const gameBoard = (() => {
-    //initialization player
+    //initialization players
     const player1 = Player('Maa', 'fire', true);
     const player2 = Player('Polo', 'water', false);
     let winner = null;
     scorePlayer1 = 0;
     scorePlayer2 = 0;
     //nb of turns
-    let turn = 9;
+    let numberTurn = 9;
     //use for changing background color of the cell
-    let theWinningCombo = [];
+    let winningCombination = [];
     //every winning combination
     const winningArray = [
         [0, 1, 2],
@@ -29,12 +29,12 @@ const gameBoard = (() => {
     //nb of columns and rows of the board
     let _colAndRow = 3;
     //get the html board
-    let boxes = document.querySelector('.boxes');
+    let boxContainer = document.querySelector('.boxes');
     //initialization of index for each cell
     let index = 0;
     //set the css property of the html board
-    boxes.style.setProperty('--temp-column', _colAndRow);
-    boxes.style.setProperty('--temp-row', _colAndRow);
+    boxContainer.style.setProperty('--temp-column', _colAndRow);
+    boxContainer.style.setProperty('--temp-row', _colAndRow);
     
     //create the board in the array and the html
     for (i = 0; i < (_colAndRow * _colAndRow); i++) {
@@ -42,13 +42,13 @@ const gameBoard = (() => {
         let cell = document.createElement('div');
         cell.classList.add('box');
         cell.setAttribute('data-index', index);
-        boxes.appendChild(cell);
+        boxContainer.appendChild(cell);
         index++;
     }
     //first feature, trigger event listener on cell of the board (one by one)
     const changePlayerTurn = (function () {
-        const gameBoxes = document.querySelectorAll('.box');
-        gameBoxes.forEach(box => {
+        const allBoxes = document.querySelectorAll('.box');
+        allBoxes.forEach(box => {
             box.addEventListener('click', e => {
                 const index = e.target.getAttribute('data-index');
                 if (player1.turn == true && winner == null) {
@@ -69,13 +69,13 @@ const gameBoard = (() => {
             });
         });
         return {
-            gameBoxes
+            allBoxes
         }
     })();
 
     //second feature, check if either players have one of the winning combinations
     checkWinner = () => {
-        turn--;
+        numberTurn--;
         let fireMoves = [];
         let waterMoves = [];
         //check elements inside the board array and move the index of the element inside either fire array or water move
@@ -89,26 +89,26 @@ const gameBoard = (() => {
         //compare the each winning combo with elements inside fire array and water array
         winningArray.forEach(combo => {
             if (combo.every(element => fireMoves.includes(element))) {
-                winner = 'player1';
-                theWinningCombo = combo;
+                gameBoard.winner = 'player1';
+                gameBoard.winningCombination = combo;
             } else if (combo.every(element => waterMoves.includes(element))) {
-                winner = 'player2';
-                theWinningCombo = combo;
-            } else if (winner == null && winner == undefined && turn == 0) {
-                winner = 'tie';
-                theWinningCombo = combo;
+                gameBoard.winner = 'player2';
+                gameBoard.winningCombination = combo;
+            } else if (winner == null && winner == undefined && numberTurn <= 0) {
+                gameBoard.winner = 'tie';
+                gameBoard.winningCombination = combo;
             }
         });
-        showRoundDisplay();
-        return theWinningCombo;
+        displayController.displayWinnerRound();
+        return winningCombination;
     }
 
     //third feature, reset any important variables to start a new round
-    resetGame = () => {
-        if (winner === 'player1') {
+    resetBoard = () => {
+        if (gameBoard.winner === 'player1') {
             player1.turn = false;
             player2.turn = true;
-        } else if (winner === 'player2') {
+        } else if (gameBoard.winner === 'player2') {
             player1.turn = true;
             player2.turn = false;
         } else {
@@ -120,97 +120,112 @@ const gameBoard = (() => {
                 player2.turn = false;
             }
         }
-        winner = null;
-        theWinningCombo = undefined;
+        gameBoard.winner = null;
+        gameBoard.winningCombination = undefined;
         
-        turn = 9;
+        numberTurn = 9;
         board.splice(0, board.length);
-        for (let i = 0; i < boxes.children.length; i++) {
-            //change background color of cells to no color
-            boxes.children[i].classList.remove('fire');
-            boxes.children[i].classList.remove('water');
-            boxes.children[i].classList.remove('winningCombo');
+        for (let i = 0; i < boxContainer.children.length; i++) {
+            //change background color of cells to default cell color
+            boxContainer.children[i].classList.remove('fire');
+            boxContainer.children[i].classList.remove('water');
+            boxContainer.children[i].classList.remove('winningCombo');
+            boxContainer.children[i].classList.remove('none');
         }
     }
-
-    //display part
-    
-    //fourth feature, display the round screen and the winning combo
-    const gameBoxes = document.querySelectorAll('.box');
-    const endRoundScreen = document.querySelector('.end-round-message');
-    const endRoundText = document.getElementById('round-text');
-    const reloadGameScreen = document.querySelector('.reload-game-message');
-    const reloadGameText = document.getElementById('winning-text');
-    
-    //fifth feature, add score to players
-    const score1 = document.getElementById('span1');
-    const score2 = document.getElementById('span2');
-    showRoundDisplay = () => {
-        //first => show the winning combination
-        showWinningCombo = () => {
-            theWinningCombo.forEach((item, index) => {
-                const boxToColor = gameBoxes.item(item);
-                boxToColor.classList.add('winningCombo');
-            });
-        };
-        //second => show the winner or tie
-        if(winner === 'player1') {
-            endRoundText.innerText = 'Player1 win!';
-            scorePlayer1 += 1;
-            score1.textContent = scorePlayer1;
-            showWinningCombo();
-        } else if (winner === 'player2') {
-            endRoundText.innerText = 'Player2 win!';
-            scorePlayer2 += 1;
-            score2.innerText = scorePlayer2;
-            showWinningCombo();
-        } else if (winner === 'tie') {
-            endRoundText.innerText = "It's a tie";
-        } else {
-            return;
-        };
-        //third, display next round screen or end game screen
-        if (scorePlayer1 < 5 && scorePlayer2 < 5) {
-            endRoundScreen.classList.add('show');
-        } else {
-            showTheFinalWinner(scorePlayer1, scorePlayer2);
-        }
-    }
-    
-    //sixth feature, click on button to start next round and remove round message
-    removeRoundDisplay = () => {
-        resetGame();
-        endRoundScreen.classList.remove('show');
-    }
-
-    //seventh feature, first to 5points win the game and reload game
-    showFinalDisplay = (scoreP1, scoreP2) => {
-        if (scoreP1 >= 5) {
-            reloadGameScreen.classList.add('show');
-            reloadGameText.innerText = "Player1 is the Champion!";
-        }else if (scoreP2 >= 5) {
-            reloadGameScreen.classList.add('show');
-            reloadGameText.innerText = "Player2 is the Champion!";
-        } else {
-            return;
-        }
-    }
-
-    const restartBtn = document.getElementById('restart');
-    restartBtn.addEventListener('click', removeRoundDisplay);
-
-    const reloadBtn = document.getElementById('reload');
-    reloadBtn.addEventListener('click', () => { location.reload(); });
 
     return {    
         board,
         changePlayerTurn, 
         checkWinner, 
-        resetGame, 
-        theWinningCombo,
+        resetBoard, 
+        winningCombination,
         winner,
-        showRoundDisplay,
-        removeRoundDisplay,
-        showFinalDisplay
+        scorePlayer1,
+        scorePlayer2,
+        boxContainer
+    }
+})();
+
+const displayController = (() => {
+    
+    //fourth feature, display the round screen and the winning combo
+    const allBoxes = document.querySelectorAll('.box');
+    const roundDisplay = document.querySelector('.end-round-message');
+    const roundDisplayText = document.getElementById('round-text');
+    const finalDisplay = document.querySelector('.reload-game-message');
+    const finalText = document.getElementById('winning-text');
+    //first => show the winning combination
+    displayWinCombo = () => {
+        gameBoard.winningCombination.forEach((item, index) => {
+            const boxToColor = allBoxes.item(item);
+            boxToColor.classList.add('winningCombo');
+        });
+    }
+    //fifth feature, add score to players
+    const scoreP1 = document.getElementById('span1');
+    const scoreP2 = document.getElementById('span2');
+    displayWinnerRound = () => {
+        //second => show the winner or tie
+        if(gameBoard.winner === 'player1') {
+            roundDisplayText.innerText = 'Player1 win!';
+            gameBoard.scorePlayer1 += 1;
+            scoreP1.textContent = gameBoard.scorePlayer1;
+            displayWinCombo();
+        } else if (gameBoard.winner === 'player2') {
+            roundDisplayText.innerText = 'Player2 win!';
+            gameBoard.scorePlayer2 += 1;
+            scoreP2.innerText = gameBoard.scorePlayer2;
+            displayWinCombo();
+        } else if (gameBoard.winner === 'tie') {
+            roundDisplayText.innerText = "It's a tie";
+        } else {
+            return;
+        };
+        //third, display next round screen or end game screen
+        if (gameBoard.scorePlayer1 < 5 && gameBoard.scorePlayer2 < 5) {
+            roundDisplay.classList.add('show');
+            disableAllBoxes();
+        } else {
+            displayWinnerGame(gameBoard.scorePlayer1, gameBoard.scorePlayer2);
+            disableAllBoxes();
+        }
+    }
+
+    disableAllBoxes = () => {
+        for (let i = 0; i < gameBoard.boxContainer.children.length; i++) {
+            gameBoard.boxContainer.children[i].classList.add('none');
+        }
+    }
+    
+    //sixth feature, click on button to start next round and remove round message
+    removeDisplay = () => {
+        gameBoard.resetBoard();
+        roundDisplay.classList.remove('show');
+    }
+
+    //seventh feature, first to 5points win the game and reload game
+    displayWinnerGame = (scoreP1, scoreP2) => {
+        if (scoreP1 >= 5) {
+            finalDisplay.classList.add('show');
+            finalText.innerText = "Player1 is the Champion!";
+        }else if (scoreP2 >= 5) {
+            finalDisplay.classList.add('show');
+            finalText.innerText = "Player2 is the Champion!";
+        } else {
+            return;
+        }
+    }
+
+    const nextRoundBtn = document.getElementById('new-round');
+    nextRoundBtn.addEventListener('click', removeDisplay);
+
+    const reloadBtn = document.getElementById('reload');
+    reloadBtn.addEventListener('click', () => { location.reload(); });
+    return {
+        displayWinnerRound,
+        removeDisplay,
+        displayWinnerGame,
+        disableAllBoxes
     }
 })();
